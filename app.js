d99a24f482337c8b4a98df89ef5d34b2f2a78f9b
Angel Cahill <angelcahill@gmail.com>
2018-10-18 00:31:07 -0400

    additional info

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ data.forEach(function(sighting) {
    });
  });
 
+ 
 // // Step 5: Use d3 to update each cell's text with
 // // ufo report values (date, city, state, country, shape, duration, comments)
 data.forEach(function(sighting) {
@@ -52,6 +53,31 @@ data.forEach(function(sighting) {
      cell.text(value);
   });
  });
+
+ // Assign the data from `data.js` to a descriptive variable
+var alien = data;
+
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(alien);
+
+  var filteredData = alien.filter(alien => alien.datetime === inputValue);
+
+  console.log(filteredData);
+
 
 // BONUS: Refactor to use Arrow Functions!
 data.forEach((sighting) => {
