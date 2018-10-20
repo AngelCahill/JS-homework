ed0a4eee2fccbb80c995daa4e440a436788ca538
Angel Cahill <angelcahill@gmail.com>
2018-10-20 14:28:40 -0400

    final

--- app.js
+++ app.js
@@ -55,28 +55,29 @@ data.forEach(function(sighting) {
  });
 
  // Assign the data from `data.js` to a descriptive variable
-// var alien = data;
+ var alien = data;
 
 // // Select the submit button
-// var submit = d3.select("#filter-btn");
-
-// submit.on("click", function() {
+var submit = d3.select("#filter-btn");
+ submit.on("click", function() {
 
 //   // Prevent the page from refreshing
-//   d3.event.preventDefault();
+  d3.event.preventDefault();
 
-//   // Select the input element and get the raw HTML node
-//   var inputElement = d3.select("#datetime");
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
 
-//   // Get the value property of the input element
-//   var inputValue = inputElement.property("value");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
 
-//   console.log(inputValue);
-//   console.log(alien);
+  console.log(inputValue);
+  console.log(alien);
 
-//   var filteredData = alien.filter(alien => alien.datetime === inputValue);
+  var filteredData = alien.filter(alien => alien.datetime === inputValue);
 
-//   console.log(filteredData);
+  console.log(filteredData);
+ });
+
 
 
 // BONUS: Refactor to use Arrow Functions!
@@ -87,3 +88,4 @@ data.forEach((sighting) => {
     cell.text(value);
   });
 });
+
